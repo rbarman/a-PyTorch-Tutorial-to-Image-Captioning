5928d7acd82bb136300dde74fe61b90f5cee52a8
Rohan Barman <rohan1barman@gmail.com>
2020-08-28 23:12:00 -0500

    print seq

--- caption.py
+++ caption.py
@@ -215,5 +215,7 @@ if __name__ == '__main__':
     seq, alphas = caption_image_beam_search(encoder, decoder, args.img, word_map, args.beam_size)
     alphas = torch.FloatTensor(alphas)
 
+    print(seq)
+
     # Visualize caption and attention of best sequence
     visualize_att(args.img, seq, alphas, rev_word_map, args.smooth)
